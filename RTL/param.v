1c9b91ccb0794b37cf920498bc89c349d80413f1
BonseoKoo <kooseo1296@gmail.com>
2026-08-22 13:21:39 +0900

    Add files via upload

---
+++ RTL/param.v
@@ -0,0 +1,14 @@
+// PE Parameters
+`define PE_ROW  9
+`define PE_COL  16
+`define BIT_ROW_ID  4
+`define BIT_VALID   1
+
+// data
+`define BIT_DATA    8
+`define BIT_PSUM    24
+
+// SRAM Address widths
+`define ADDR_ASRAM 10
+`define ADDR_WSRAM  9
+`define ADDR_PSRAM 10
